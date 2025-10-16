64218ea347896f8e4a3c662718df5584edd779a9
Alexander Haas <Haa240104@spengergasse.at>
2025-10-16 16:09:33 +0200

    HÜ fertig 2025-10-16

--- debug_w_prim-n/main_test.ts
+++ debug_w_prim-n/main_test.ts
@@ -2,5 +2,21 @@ import { assertEquals } from "@std/assert";
 import { is_it_prime } from "./main.ts";
 
 Deno.test(function is_it_primeTest() {
-  assertEquals(is_it_prime(3), true);
+  assertEquals(is_it_prime(1), [true, "Ja, 1 ist eine Primzahl"]);
+});
+
+Deno.test(function is_it_primeTest() {
+  assertEquals(is_it_prime(4), [false, "Nein, 4 ist keine Primzahl"]);
+});
+
+Deno.test(function is_it_primeTest() {
+  assertEquals(is_it_prime(9), [false, "Nein, 9 ist keine Primzahl"]);
+});
+
+Deno.test(function is_it_primeTest() {
+  assertEquals(is_it_prime(22), [false, "Nein, 22 ist keine Primzahl"]);
+});
+
+Deno.test(function is_it_primeTest() {
+  assertEquals(is_it_prime(23), [true, "Ja, 23 ist eine Primzahl"]);
 });
