6bd89294c7d380602faa15b1ec0b01b83f8671f2
Alexander Haas <Haa240104@spengergasse.at>
2026-01-26 12:18:55 +0100

    lambda HÜ

---
+++ 2026-01-19_lambda/lambda.ts
@@ -0,0 +1,21 @@
+const persons = [
+  { name: "Marie", age: 14, gender: "f" },
+  { name: "Tim", age: 15, gender: "m" },
+  { name: "Max", age: 15, gender: "m" },
+  { name: "Leonie", age: 13, gender: "f" },
+  { name: "Niki", age: 16, gender: "m" }
+];
+
+persons.sort((a, b) => a.age - b.age);
+
+console.log("Nach Alter sortiert:");
+console.log(persons);
+
+const males = persons.filter(person => person.gender === "m");
+const females = persons.filter(person => person.gender === "f");
+
+console.log("Männliche Personen:");
+console.log(males);
+
+console.log("Weibliche Personen:");
+console.log(females);
